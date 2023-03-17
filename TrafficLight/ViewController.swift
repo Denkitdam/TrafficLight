ad6f27407e9ddc123a32d376d14db2d50e0f7eab
denkitdam <cryptoden145@gmail.com>
2023-03-17 23:44:03 +0300

    IBOutlets connected

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightLabel: UIView!
+    @IBOutlet var yellowLightLabel: UIView!
+    @IBOutlet var greenLightLabel: UIView!
+    @IBOutlet var switchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonWasTapped() {
+    }
+    
 }
 
